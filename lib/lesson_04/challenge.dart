dd54f09124614ecc2d97d832187197e357400623
ShokseDev <aleshchenko.daniil@yandex.com>
2025-05-16 17:16:19 +0300

    feat(dart): completed lesson 4 - loops

---
+++ lib/lesson_04/challenge.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: avoid_print
+
+void main() {
+  // List of my favorite films
+  var films = [
+    'Jagten',
+    'En man som heter Ove',
+    'Le Fabuleux Destin d\'Amélie Poulain',
+    'Alien',
+    'The Electrical life of Louis Wain',
+  ];
+
+  // Enumeration with indexes
+  for (var i = 0; i < films.length; i++) {
+    print('Film #${i + 1}: ${films[i]}');
+  }
+
+  // Simple enumeration
+  for (var films in films) {
+    print(films);
+  }
+
+  // Film search
+  final searchedFilm = 'Alien';
+  int index = 0;
+
+  while (index < films.length) {
+    if (films[index] == searchedFilm) {
+      print('There it is!');
+      break;
+    } else {
+      print('Searching');
+      index++;
+    }
+  }
+}
